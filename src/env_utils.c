48b8b1f59ab774e90a1c9a421e12f18e2183c189
Fourse <pozharsky.g@gmail.com>
2019-09-26 20:06:33 +0300

    env_utils

--- src/env_utils.c
+++ src/env_utils.c
@@ -6,18 +6,31 @@
 /*   By: rloraine <rloraine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/25 22:10:52 by rloraine          #+#    #+#             */
-/*   Updated: 2019/09/25 22:16:50 by rloraine         ###   ########.fr       */
+/*   Updated: 2019/09/25 22:35:08 by rloraine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	set_env(char **argv, char **env)
+#define MYERROR 1
+
+// void	unset_env(char **argv, char **env)
+// {
+	
+// }
+
+// void	set_env(char **argv, char **env)
+// {
+	
+// }
+
+int		check_comm(t_argv *beg)
 {
 	
 }
 
-void	get_env(t_argv *beg, int argc, char **argv, char **env)
+void	get_env(t_argv *beg, char **env)
 {
-	
+	if (!check_comm(beg))
+		exit(MYERROR);
 }
