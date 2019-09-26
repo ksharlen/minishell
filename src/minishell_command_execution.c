e4fa51dacde97b4f346bc242cab555fcee4ce98e
Kharazim Sharlene <ksharlen@mi-t2.21-school.ru>
2019-09-26 15:56:03 +0300

    added new file minishell_command_search.c

--- src/minishell_command_execution.c
+++ src/minishell_command_execution.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 15:44:51 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/09/25 23:20:59 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/09/26 15:30:10 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,32 @@ void	minishell_command_execution(t_argv *beg, char *const env[])
 {
 	pid_t 	pid;
 	int		status_child;
-	char	*path_ex;
+	char	path_ex[MAX_SIZE_PATH + 1];
+	int		ret_minishell_cmd_search;
 
-	path_ex = NULL;
 	while (beg)
 	{
-		pid = fork();
-		if (!pid)
+		ret_minishell_cmd_search = minishell_command_search(beg->argv[0], path_ex);
+		if (ret_minishell_cmd_search == FOUND_INTERNAL_DIR)
 		{
-			path_ex = ft_strjoin(FULL_PATH, beg->argv[0]);
-			execve(path_ex, beg->argv, env);
-			//working env
-			exit(EXIT_SUCCESS);
+			pid = NEW_PROCESS();
+			if (!pid)
+			{
+				{
+					if (execve(path_ex, beg->argv, env) == NOT_FOUND)
+						CMD_NOT_FOUND(beg->argv[0]);
+					else
+						exit(EXIT_SUCCESS);
+				}
+				//working env
+			}
+			else
+			{
+				wait(&status_child);
+			}
 		}
-		else
-		{
-			wait(&status_child);
-		}
-		
-		ft_strdel(&path_ex);
+		else if (ret_minishell_cmd_search == NOT_FOUND)
+			CMD_NOT_FOUND(beg->argv[0]);
 		beg = beg->next;
 	}
 }
---
+++ src/minishell_command_search.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_command_search.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/26 14:50:39 by ksharlen          #+#    #+#             */
+/*   Updated: 2019/09/26 15:13:42 by ksharlen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	find_from_interlnal_dir_ex(const char *cmd)
+{
+	char	**p_internal_cmd;
+
+	p_internal_cmd = g_internal_commands;
+	while (*p_internal_cmd)
+	{
+		if (!ft_strcmp(*p_internal_cmd, cmd))
+			return (FOUND_INTERNAL_DIR);
+		++p_internal_cmd;
+	}
+	return (NOT_FOUND);
+}
+
+int		minishell_command_search(const char *cmd, char *path_ex)
+{
+	ft_bzero(path_ex, MAX_SIZE_PATH + 1);
+	if (cmd && path_ex && *cmd)
+	{
+		if (find_from_interlnal_dir_ex(cmd) == FOUND_INTERNAL_DIR)
+		{
+			ft_strcpy(path_ex, INTERNAL_PATH_CMD);
+			ft_strcat(path_ex, cmd);
+			return (FOUND_INTERNAL_DIR);
+		}
+		// else if (find_from_path_env(cmd) == FOUND)
+		// 	ft_strcpy(path_ex, )
+	}
+	//сначала ищем в нашем каталоге
+	//потом ищем в каталогах PATH
+	//Если не нашли нигде, возвращаем -1.
+	return (NOT_FOUND);
+}
