57b76f99376e9f1fc929ef163b0b10c41953b233
Kharazim Sharlene <ksharlen@mi-i3.21-school.ru>
2019-10-02 21:49:39 +0300

    normenete

--- src/minishell_push_minishell_history.c
+++ src/minishell_push_minishell_history.c
@@ -6,19 +6,19 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 16:45:31 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/10/02 20:51:11 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/10/02 21:47:57 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void			getkey_internal(struct s_key_data *key)
+void		getkey_internal(struct s_key_data *key)
 {
 	key->key_str = R_FVAL(MRC, KEY_LAST_CMD_MHISTORY);
 	key->key = ft_atoi(key->key_str);
 }
 
-static void		getdate_internal(char *date_ex_cmd)
+static void	getdate_internal(char *date_ex_cmd)
 {
 	time_t		date_ex;
 	char		*str_date;
@@ -30,24 +30,20 @@ static void		getdate_internal(char *date_ex_cmd)
 	ft_memcpy(date_ex_cmd, str_date, ft_strlen(str_date) - 1);
 }
 
-static void push_mhistory(struct s_key_data *k_data, const char *cmd_str)
-{
-	if (cmd_str && *cmd_str && k_data->fd_ms_history != -1)
-		ft_printf("%v%d:[%s];%s\n", k_data->fd_ms_history, k_data->key, k_data->date_ex_cmd, cmd_str);
-}
-
-void			minishell_push_minishell_history(const char *str_stdio, struct s_key_data *k_data)
+void		minishell_push_minishell_history(const char *str_stdio,
+	struct s_key_data *k_data)
 {
 	if (str_stdio && k_data && *str_stdio && k_data->key_str)
 	{
 		ft_bzero(k_data->date_ex_cmd, SIZE_DATE + 1);
 		getdate_internal(k_data->date_ex_cmd);
-		push_mhistory(k_data, str_stdio);
+		ft_printf("%v%d:[%s];%s\n", k_data->fd_ms_history,
+			k_data->key, k_data->date_ex_cmd, str_stdio);
 		k_data->key++;
 	}
 }
 
-void			minishell_history_close(struct s_key_data *k_data)
+void		minishell_history_close(struct s_key_data *k_data)
 {
 	char *key;
 
@@ -61,7 +57,7 @@ void			minishell_history_close(struct s_key_data *k_data)
 	ft_strdel(&key);
 }
 
-void			minishell_history_init(struct s_key_data *k_data)
+void		minishell_history_init(struct s_key_data *k_data)
 {
 	mode_t		st_mode;
 	int			fd;
@@ -71,7 +67,8 @@ void			minishell_history_init(struct s_key_data *k_data)
 	k_data->key_str = R_FVAL(MRC, KEY_LAST_CMD_MHISTORY);
 	if (!k_data->key_str)
 	{
-		if ((fd = open(MRC, O_CREAT | O_WRONLY | O_APPEND, st_mode)) == RET_ERROR)
+		if ((fd = open(MRC, O_CREAT | O_WRONLY | O_APPEND,
+			st_mode)) == RET_ERROR)
 			err_exit(E_OPEN, "minishell");
 		ft_printf("%v%s=%s", fd, KEY_LAST_CMD_MHISTORY, "0");
 		k_data->key = 0;
@@ -81,6 +78,7 @@ void			minishell_history_init(struct s_key_data *k_data)
 	}
 	else
 		k_data->key = ft_atoi(k_data->key_str);
-	if ((k_data->fd_ms_history = open(MHISTORY, O_CREAT | O_WRONLY | O_APPEND, st_mode)) == RET_ERROR)
+	if ((k_data->fd_ms_history = open(MHISTORY, O_CREAT | O_WRONLY | O_APPEND,
+		st_mode)) == RET_ERROR)
 		err_exit(E_OPEN, "minishell");
 }
