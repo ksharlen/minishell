7091f31b3916705179a64bfbbe331ff4e09fbda5
Fourse <pozharsky.g@gmail.com>
2019-10-04 20:30:26 +0300

    Sanhya prosti

--- src/cd.c
+++ src/cd.c
@@ -6,7 +6,7 @@
 /*   By: rloraine <rloraine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 22:35:31 by rloraine          #+#    #+#             */
-/*   Updated: 2019/10/04 20:13:49 by rloraine         ###   ########.fr       */
+/*   Updated: 2019/10/04 20:30:05 by rloraine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,63 +29,64 @@ static char	*find_env(char *env[], char *key)
 static void	back_step(char *cur_dir, char **full_path)
 {
 	char	*new_dir;
-	int		i;
 	int		new_len;
 
-	i = -1;
 	new_len = 0;
 	while (cur_dir[new_len])
 		++new_len;
 	while (cur_dir[new_len] != '/')
 		--new_len;
 	new_dir = ft_strsub(cur_dir, 0, new_len);//!need to defense
-	new_dir[i] = '\0';
-	(*full_path) = new_dir;
+	(*full_path) = ft_strdup(new_dir);
 	ft_strdel(&new_dir);
 }
 
-static int	check_access(char *argv[], char **check_path)
-{
-	if (!(access(check_path, F_OK)))
-	{
-		if (access(check_path, W_OK))
-		{
-			ft_strdel(check_path);
-			PRINT_ERROR_AND_RET(argv[0], PERM_DENIED, argv[1]);
-		}
-		ft_strdel(check_path);
-		return (0);
-	}
-	ft_strdel(check_path);
-	PRINT_ERROR_AND_RET(argv[0], NO_SUCH_F_OR_D, argv[1]);
-}
+// static int	check_access(char *argv[], char **check_path)
+// {
+// 	if (!(access(*check_path, F_OK)))
+// 	{
+// 		if (access(*check_path, W_OK))
+// 		{
+// 			ft_strdel(check_path);
+// 			PRINT_ERROR_AND_RET(argv[0], PERM_DENIED, argv[1]);
+// 		}
+// 		ft_strdel(check_path);
+// 		return (0);
+// 	}
+// 	ft_strdel(check_path);
+// 	PRINT_ERROR_AND_RET(argv[0], NO_SUCH_F_OR_D, argv[1]);
+// }
 
-static int	valid(char *argv[], char *home_dir)
-{
-	int		i;
-	int		check;
-	char	*check_path;
+//!VAlIDACIYA IZ GOVNA YA HZ KAK UZNAT' POLNIY PUT', DA YA TUPOY
+//!оно причем когда как, когда работает, когда нет, соре(((((
 
-	i = -1;
-	check = 0;
-	if (!argv)
-		return (0);
-	if (!ft_strstr(argv[1], home_dir))
-		check_path = ft_strjoin(home_dir, argv[1] + 1);//!need to defense
-	else
-		check_path = ft_strdup(argv[1]);//!need to defense
-	while (argv[1][++i] && ++check)
-	{
-		if (i == MAX_SIZE_PATH)
-			PRINT_ERROR_AND_RET(argv[0], NO_SUCH_F_OR_D, argv[1]);
-		if (argv[1][i] == '/')
-			if (check > MAX_UNAME)
-				PRINT_ERROR_AND_RET(argv[0], FILE_TO_LONG, argv[1]);
-	}
-	if (!(check_access(argv, &check_path)))
-		return (0);
-	PRINT_ERROR_AND_RET(argv[0], NO_SUCH_F_OR_D, argv[1]);
-}
+// static int	valid(char *argv[], char **full_path, char *home_dir)
+// {
+// 	int		i;
+// 	int		check;
+// 	char	*check_path;
+
+// 	i = -1;
+// 	check = 0;
+// 	P_UNUSED(full_path);
+// 	if (!argv)
+// 		return (0);
+// 	if (!ft_strstr(argv[1], home_dir))
+// 		check_path = ft_strjoin(home_dir, argv[1] + 1);//!need to defense
+// 	else
+// 		check_path = ft_strdup(argv[1]);//!need to defense
+// 	while (argv[1][++i] && ++check)
+// 	{
+// 		if (i == MAX_SIZE_PATH)
+// 			PRINT_ERROR_AND_RET(argv[0], NO_SUCH_F_OR_D, argv[1]);
+// 		if (argv[1][i] == '/')
+// 			if (check > MAX_UNAME)
+// 				PRINT_ERROR_AND_RET(argv[0], FILE_TO_LONG, argv[1]);
+// 	}
+// 	if (!(check_access(argv, &check_path)))
+// 		return (0);
+// 	PRINT_ERROR_AND_RET(argv[0], NO_SUCH_F_OR_D, argv[1]);
+// }
 
 static int	check_first_arg(char *argv[], char *env[], char *home_dir,\
 char **full_path)
@@ -96,11 +97,11 @@ char **full_path)
 	tmp = find_env(env, "OLDPWD=");//!need to defense
 	getcwd(cur_dir, MAX_SIZE_PATH);//!need to defense
 	if (!argv[1] || ft_strequ(argv[1], "~") || ft_strequ(argv[1], "--"))
-		(*full_path) = home_dir;
+		(*full_path) = ft_strdup(home_dir);
 	else if (ft_strstr(argv[1], "~/"))
 		(*full_path) = ft_strjoin(home_dir, argv[1] + 1);//!need to defense
 	else if (ft_strequ(argv[1], "."))
-		my_setenv("OLDPWD", cur_dir, NULL);
+		(*full_path) = ft_strdup(cur_dir);
 	else if (ft_strequ(argv[1], ".."))
 		back_step(cur_dir, full_path);
 	else if (ft_strequ(argv[1], "-"))
@@ -108,8 +109,8 @@ char **full_path)
 	else
 	{
 		(*full_path) = argv[1];
-		if (valid(argv, home_dir))
-			return (-1);
+		// if (valid(argv, full_path, home_dir))
+		// 	return (-1);
 	}
 	ft_strdel(&tmp);
 	return (0);
@@ -137,5 +138,7 @@ int			cd(int argc, char *argv[], char *env[])
 		my_setenv("PWD", full_path, cur_dir);
 	}
 	ft_strdel(&home_dir);
+	if (ft_strequ(argv[1], "-"))
+		pwd(argc, argv, env);
 	return (0);
 }
