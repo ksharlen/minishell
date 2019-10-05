7a3e9dd55faf505c7fb2bdc40a68ab2589404217
Kharazim Sharlene <ksharlen@mi-u3.21-school.ru>
2019-10-05 19:14:05 +0300

    new update unsetenv

--- src/internal_utilities/main_test.c
+++ src/internal_utilities/main_test.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 16:29:05 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/10/05 18:39:24 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/10/05 19:12:54 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@ int		main(void)
 
 	ret_setenv = minishell_unsetenv("LESS");
 	if (ret_setenv == SUCCESS)
-		ft_printf("\e[32;1m========SUCCESS=========\n");
+		ft_printf("========SUCCESS=========\n");
 	else if (ret_setenv == FAILURE)
-		ft_printf("\e[31;1m========FAILURE=========\n");
-	ft_print_lines(environ);
+		ft_printf("========FAILURE=========\n");
+	ft_printf("len_env[2] = %d\n", environ[0][2]);
+	// ft_print_lines(environ);
 	return (0);
 }
