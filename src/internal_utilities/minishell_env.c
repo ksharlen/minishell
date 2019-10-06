d520628805a3235866b4536b6b644d7d980209ad
Kharazim Sharlene <ksharlen@vo-g2.21-school.ru>
2019-10-06 16:27:16 +0300

    new update

--- src/internal_utilities/minishell_env.c
+++ src/internal_utilities/minishell_env.c
@@ -1,29 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_test.c                                        :+:      :+:    :+:   */
+/*   minishell_env.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/05 16:29:05 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/10/06 15:15:53 by ksharlen         ###   ########.fr       */
+/*   Created: 2019/10/06 15:16:22 by ksharlen          #+#    #+#             */
+/*   Updated: 2019/10/06 15:16:28 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "internal_utilities.h"
 
-int		main(void)
-{
-	int ret_setenv;
-	size_t	len_env;
-
-	ret_setenv = minishell_unsetenv("");
-	if (ret_setenv == SUCCESS)
-		ft_printf("\e[32;1m========SUCCESS=========\n");
-	else if (ret_setenv == FAILURE)
-		ft_printf("\e[31;1m========FAILURE=========\n");
-	// ft_printf("len_env[2] = %d\n", environ[0][2]);
-	len_env = ft_print_lines(environ);
-	ft_printf("len_env: %ld\n", len_env);
-	return (0);
-}
