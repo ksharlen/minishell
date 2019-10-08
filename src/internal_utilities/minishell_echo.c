ad90f42913ed02ca130f2f7e4a5c2575101b59dc
Kharazim Sharlene <ksharlen@mi-u3.21-school.ru>
2019-10-08 21:30:29 +0300

    normenett echo

--- src/internal_utilities/minishell_echo.c
+++ src/internal_utilities/minishell_echo.c
@@ -6,24 +6,11 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 20:25:02 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/10/08 20:49:36 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/10/08 20:55:34 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "internal_utilities.h"
-
-// static int	get_opt(int argc, char *const argv[])
-// {
-// 	int	ch;
-
-// 	ch = 0;
-// 	if (argv)
-// 	{
-// 		while ((ch = getopt(argc, argv, ECHO_OPT)) != -1)
-// 			;
-// 	}
-// 	return (SUCCESS);
-// }
 
 void		work_echo(char *const argv[], unsigned char *flag)
 {
@@ -34,15 +21,12 @@ void		work_echo(char *const argv[], unsigned char *flag)
 	}
 	ft_printf("%s", *argv);
 	*flag == FLAG_ON ? ft_printf("%%\n") : ft_printf("\n");
-	// if (*flag == FLAG_OFF)
-	// 	ft_printf("\n");
-	// else
-	// 	ft_printf("%%\n");
 }
 
 int			minishell_echo(int argc, char **argv, char **env)
 {
-	unsigned char flag;
+	unsigned char	flag;
+
 	P_UNUSED(env);
 	flag = FLAG_OFF;
 	if (argc == 1)
